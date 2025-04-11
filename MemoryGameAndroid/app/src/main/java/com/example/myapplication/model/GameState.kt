7030230f6ce9d89963a307c04487b4ef733669df
Erick <persona0rcc@gmail.com>
2025-04-11 18:45:39 -0300

    ConstrutoresCardGameStatePlayer

--- MemoryGameAndroid/app/src/main/java/com/example/myapplication/model/GameState.kt
+++ MemoryGameAndroid/app/src/main/java/com/example/myapplication/model/GameState.kt
@@ -1,1 +1,13 @@
 package com.example.myapplication.model
+
+sealed class GameState {
+    object Menu : GameState()
+    data class Playing(
+        val board: List<List<Card>>,
+        val players: List<Player>,
+        val currentPlayerIndex: Int,
+        val firstSelectedCard: Pair<Int, Int>?,
+        val secondSelectedCard: Pair<Int, Int>?
+    ) : GameState()
+    data class GameOver(val winner: Player) : GameState()
+}
--- MemoryGameAndroid/app/src/main/java/com/example/myapplication/model/Player.kt
+++ MemoryGameAndroid/app/src/main/java/com/example/myapplication/model/Player.kt
@@ -1,1 +1,6 @@
 package com.example.myapplication.model
+data class Player(
+    val name: String,
+    val color: String,
+    var score: Int = 0
+)
